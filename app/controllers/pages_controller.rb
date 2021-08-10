a6059c30fffcd55b0bfefa6e2d9601edce80b632
Tiago Clarenc <tiagoclarenc888@gmail.com>
2021-08-10 17:25:10 +0400

    routes implemented as well as dashboard action for applications

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,8 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def dashboard
+    @my_applications = current_user.applications
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "dashboard", to: "pages#dashboard", as: :dashboard
+  resources :organisations do
+    resources :jobs, only: [:new, :create]
+  end
+  resources :jobs, except: [:new, :create]
+  resources :qualifications, except: [:show, :index]
+  resources :applications, except: [:show, :edit, :update]
+  # nest interviews here when time is right
 end
