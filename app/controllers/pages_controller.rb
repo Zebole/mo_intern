213c771270a8d9e69efe3f458e3f537375d102f6
Khooshal Seechurn <khooshalseechurn@gmail.com>
2021-08-12 11:01:47 +0400

    dashboards created

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,9 +10,13 @@ class PagesController < ApplicationController
 
   def employer_dashboard
     @my_jobs = current_user.jobs
+    @my_count = current_user.jobs.count
+    @my_applications = current_user.jobs.applications
+    @my_applicants_size = current_user.jobs.applications.count
   end
 
   def applicant_dashboard
-
+    @your_applications = current_user.applications
+    @your_count = current_user.applications.count
   end
 end
