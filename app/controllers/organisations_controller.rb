95a9eb771bf9ef67ac30d18d318d535ea114e64f
Nasreen Dowlut <nasmdow@gmail.com>
2021-08-12 10:09:39 +0100

    create_organisatiins_controllers_and_views

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -1,2 +1,48 @@
 class OrganisationsController < ApplicationController
+
+  def index
+    @organisations = Organisation.all
+  end
+
+  def show
+    @organisation = Organisation.find(params[:id])
+  end
+
+  def new
+    @organisation = Organisation.new
+  end
+
+  def create
+    @organisation = Organisation.new(organisation_params)
+    @user = current_user
+    @organisation.user = @user
+    if @organisation.save
+      redirect_to organisation_path(@organisation)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @organisation = Organisation.find(params[:id])
+  end
+
+  def update
+    @organisation = Organisation.find(params[:id])
+    @organisation.update(organisation_params)
+    redirect_to organisations_path
+  end
+
+  def destroy
+    @organisation = Organisation.find(params[:id])
+    @organisation.destroy
+    redirect_to organisations_path
+  end
+
+  private
+
+  def organisation_params
+   params.require(:organisation).permit(:name, :address, :website, :city, :phone_number, :email, :description)
+  end
+
 end
