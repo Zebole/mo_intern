8e6751934858357c57746c9ed6fbe59cb4319397
Tiago Clarenc <tiagoclarenc888@gmail.com>
2021-08-12 15:14:50 +0400

    user_qualif controller

--- app/controllers/user_qualifications_controller.rb
+++ app/controllers/user_qualifications_controller.rb
@@ -4,4 +4,13 @@ class UserQualificationsController < ApplicationController
     @user_qualification = UserQualification.new
   end
 
+  def create
+    qualification = Qualification.first
+    # qualification = Qualification.find_instance(params[:user_qualification][:qualification])
+    # u = UserQualification.new(user: current_user)
+    # u.qualification_id = qualification.id
+    # u.save!
+    raise
+  end
+
 end
--- app/models/qualification.rb
+++ app/models/qualification.rb
@@ -5,7 +5,12 @@ class Qualification < ApplicationRecord
 
   def self.to_s
     self.all.map {|qualification| "#{qualification.qualification_category} - #{qualification.qualification_value}" }
+  end
 
+  def self.find_instance(string)
+    category = string.split(" -").first
+    value = string.split("- ").last
+    self.find_by(qualification_category: category, qualification_value: value)
   end
 
 end
